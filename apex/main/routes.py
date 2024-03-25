614276f4c05964bfb4d17de55b4113969e489f47
Natnael Shiferaw <130279239+natnael-shiferaw@users.noreply.github.com>
2024-03-25 10:06:25 +0000

    update

--- apex/main/routes.py
+++ apex/main/routes.py
@@ -1,8 +1,31 @@
 from flask import render_template, request, Blueprint
-
+from apex.models import Property
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route("/home")
 def home():
-    return render_template('home.html')
+    # Fetch all properties for sale
+    for_sale = Property.query.filter_by(listing_type='sale').all()
+
+    # Fetch all properties for rent
+    for_rent = Property.query.filter_by(listing_type='rent').all()
+    return render_template('home.html', for_sale=for_sale, for_rent=for_rent)
+
+@main.route("/buy")
+def buy():
+    for_sale = Property.query.filter_by(listing_type='sale').all()
+    # Query to get the total number of properties for sale
+    total_properties_for_sale = Property.query.filter_by(listing_type='sale').count()
+    return render_template('buy.html', for_sale=for_sale, total_properties_for_sale=total_properties_for_sale)
+
+@main.route("/rent")
+def rent():
+    for_rent = Property.query.filter_by(listing_type='rent').all()
+    # Query to get the total number of properties for rent
+    total_properties_for_rent = Property.query.filter_by(listing_type='rent').count()
+    return render_template('rent.html', for_rent=for_rent, total_properties_for_rent=total_properties_for_rent)
+
+@main.route("/property_details")
+def property_details():
+    return render_template('property_details.html')
--- replit.nix
+++ replit.nix
@@ -1,5 +1,20 @@
 { pkgs }: {
     deps = [
+      pkgs.cacert
+      pkgs.zlib
+      pkgs.tk
+      pkgs.tcl
+      pkgs.openjpeg
+      pkgs.libxcrypt
+      pkgs.libwebp
+      pkgs.libtiff
+      pkgs.libjpeg
+      pkgs.libimagequant
+      pkgs.lcms2
+      pkgs.freetype
+      pkgs.rustc
+      pkgs.libiconv
+      pkgs.cargo
       pkgs.mailutils
       pkgs.cowsay
     ];
